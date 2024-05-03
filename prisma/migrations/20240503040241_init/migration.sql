35f982cb220aed1ce66277a69e8f8f869ea7aced
enjoyyourcreaditcard <joywidiwibowo@gmail.com>
2024-05-03 13:19:54 +0700

    feat(orm): add prisma migrations and seeders

---
+++ prisma/migrations/20240503040241_init/migration.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE INDEX "segments_category_id_idx" ON "segments"("category_id");
---
+++ prisma/migrations/20240503040443_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "parts_segment_id_idx" ON "parts"("segment_id");
+
+-- CreateIndex
+CREATE INDEX "parts_brand_id_idx" ON "parts"("brand_id");
